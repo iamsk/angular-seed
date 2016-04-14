2b6a304f99833d8856e2c44411633b55940708d3
iamsk <iamsk.info@gmail.com>
2016-04-14 08:31:10 +0800

    add smallest_test

---
+++ app/smallest_test/custom_test.js
@@ -0,0 +1,24 @@
+//src.js
+var pluralize = function(word, number) {
+     if(number === 1) {
+          return word;
+     }
+     else {
+          return word + "s";
+     }
+ };
+
+//framework.js
+var assert = function(a,b) {
+   if(a === b) {
+     console.log('Test passed!', a, 'equals', b);
+   }
+   else {
+     console.error('Expected', a, 'to equal', b);
+   }
+ };
+
+//spec.js
+assert(pluralize('cat', 0), 'cats');
+assert(pluralize('cat', 1), 'cat');
+assert(pluralize('cat', 2), 'cats');
---
+++ app/smallest_test/jasmine_test.js
@@ -0,0 +1,24 @@
+//src.js
+var pluralize = function(word, number) {
+     if(number === 1) {
+          return word;
+     }
+     else {
+          return word + "s";
+     }
+ };
+
+//spec.js
+describe("pluralize", function() {
+
+   it("adds an s if the number is 0", function() {
+     expect(pluralize('cat', 0)).toBe('cats');
+   });
+   it("don't add an s if the number is 1", function() {
+     expect(pluralize('cat', 1)).toBe('cat');
+   });
+   it("adds an s if the number is 2", function() {
+     expect(pluralize('cat', 2)).toBe('cats');
+   });
+
+ });
